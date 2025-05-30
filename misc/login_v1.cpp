cf2ef2d614879f0f681ec2f03074171afd2764e0
abhyuday-13 <officalac2006@gmail.com>
2025-05-30 18:38:56 +0530

    no change

--- misc/login_v1.cpp
+++ misc/login_v1.cpp
@@ -5,42 +5,42 @@ using namespace std;
 
 int main() {
 
-    string realUsername = "bigpp_13";
-    string realPassword = "abhyuday@13";
+	string realUsername = "bigpp_13";
+	string realPassword = "abhyuday@13";
 
-    string inputUsername;
-    string inputPassword;
+	string inputUsername;
+	string inputPassword;
 
-    cout << "Please enter your login credentials -> \n"
-         << "--------------------------------------" << endl;
+	cout << "Please enter your login credentials -> \n"
+	     << "--------------------------------------" << endl;
 
-    for (int i = 1; i <= 3; i++) {
+	for (int i = 1; i <= 3; i++) {
 
-        cout << "Username: ";
-        cin >> inputUsername;
+		cout << "Username: ";
+		cin >> inputUsername;
 
-        if (inputUsername == realUsername) {
-            cout << "Password: ";
-            cin >> inputPassword;
+		if (inputUsername == realUsername) {
+			cout << "Password: ";
+			cin >> inputPassword;
 
-            if (inputPassword == realPassword) {
-                cout << "Login successfull !" << endl;
-                break;
-            } else {
-                cout << "Incorrect Password!" << endl;
-            }
-        } else {
-            cout << "Incorrect Username!" << endl;
-        }
+			if (inputPassword == realPassword) {
+				cout << "Login successfull !" << endl;
+				break;
+			} else {
+				cout << "Incorrect Password!" << endl;
+			}
+		} else {
+			cout << "Incorrect Username!" << endl;
+		}
 
-        int attemptsLeft = 3 - i;
+		int attemptsLeft = 3 - i;
 
-        if (attemptsLeft > 0) {
-            cout << "Attempts Left: " << attemptsLeft << endl;
-        } else {
-            cout << "\nToo many attempts. ACCESS DENIED." << endl;
-        }
-    }
+		if (attemptsLeft > 0) {
+			cout << "Attempts Left: " << attemptsLeft << endl;
+		} else {
+			cout << "\nToo many attempts. ACCESS DENIED." << endl;
+		}
+	}
 
-    return 0;
+	return 0;
 }
